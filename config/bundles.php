8eaf61d4c5c54bb9994efa4761ea7280695ef435
Aleph Lau <aleph.lau@indabasolutions.com>
2025-12-28 17:28:36 -0800

    feat: add Generic Execution Engine bundle with RabbitMQ transport

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,5 @@
 
 return [
     //Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Pimcore\Bundle\GenericExecutionEngineBundle\PimcoreGenericExecutionEngineBundle::class => ['all' => true],
 ];
